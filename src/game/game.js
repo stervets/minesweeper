5462f47e36cd07857d1b9c2831f407801d0a362b
mike <lisovm@gmail.com>
2025-04-11 18:41:41 +0300

    const, utils

--- src/game/game.js
+++ src/game/game.js
@@ -1,36 +1,14 @@
 import {ref} from "vue";
-
-/////////// Параметры игры /////////
-const BOARD_WIDTH = 20;
-const BOARD_HEIGHT = 15;
-const BOMBS_COUNT = 30;
-////////////////////////////////////
-
-// общее количество клеток
-const BOARD_SIZE = BOARD_WIDTH * BOARD_HEIGHT;
-
-// бомб не может быть больше, чем клеток на поле
-const VALIDATED_BOMBS_COUNT = Math.min(BOMBS_COUNT, BOARD_SIZE);
-
-// состояние игры
-const GAME_STATE = {
-    PLAY: 0,
-    WIN: 1,
-    LOSE: 2
-}
-
-// состояние клетки
-const CELL_STATE = {
-    CLOSED: 0, // клетка закрыта
-    FLAGGED: 1, // на клетке стоит флаг
-    OPENED: 2 // клетка открыта
-}
-
-const MINED_CELL = 10; // признак заминированной клетки
-
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+import {
+    BOARD_WIDTH,
+    BOARD_HEIGHT,
+    VALIDATED_BOMBS_COUNT,
+    BOARD_SIZE,
+    CELL_STATE,
+    GAME_STATE,
+    MINED_CELL
+} from "./const";
+import {random} from "../utils";
 
 export default {
     setup() {
